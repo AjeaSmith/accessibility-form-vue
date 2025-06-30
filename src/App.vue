7d25c7073915faecc5f5070b286699921bc36301
ajea <smithajea@rocketmail.com>
2025-06-29 20:03:57 -0400

    add form validations with vee-validate

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,117 @@
-<script setup></script>
+<script setup>
+import { toTypedSchema } from '@vee-validate/zod'
+import * as zod from 'zod'
+
+import { useForm, useField } from 'vee-validate'
+
+const schema = zod.object({
+  firstName: zod.string({ message: 'This is required' }),
+  lastName: zod.string({ message: 'This is required' }),
+  queryType: zod.string({ message: 'Please select a query type' }),
+  message: zod.string({ message: 'This is required' }),
+  terms: zod.boolean({ message: 'To submit this form, please consent to being contacted' }),
+  email: zod
+    .string({ message: 'This is required' })
+    .email({ message: 'Please enter a valid email address' }),
+})
+
+const { handleSubmit, errors } = useForm({
+  validationSchema: toTypedSchema(schema),
+})
+
+const { value: email } = useField('email')
+const { value: firstName } = useField('firstName')
+const { value: lastName } = useField('lastName')
+const { value: queryType } = useField('queryType')
+const { value: message } = useField('message')
+const { value: terms } = useField('terms')
+
+const onSubmit = handleSubmit((values) => {
+  // show success toast
+  console.log(values)
+})
+</script>
 
 <template>
-  <!-- hi -->
-  <form action="#">
+  <form @submit="onSubmit">
     <div class="app-form">
       <h1 class="heading">Contact Us</h1>
       <fieldset>
         <div class="form-group">
           <div class="form-item" id="firstname">
             <label for="firstName">First Name *</label>
-            <input autocomplete="off" required type="text" name="firstname" id="firstName" />
+
+            <input
+              v-model="firstName"
+              type="text"
+              id="firstName"
+              name="firstName"
+              :class="{ errorField: errors.firstName }"
+            />
+
+            <span v-if="errors.firstName" class="errorMessage">{{ errors.firstName }}</span>
           </div>
           <div class="form-item" id="lastname">
             <label for="lastName">Last Name *</label>
-            <input autocomplete="off" required type="text" name="lastName" id="lastName" />
+            <input
+              v-model="lastName"
+              type="text"
+              name="lastName"
+              id="lastName"
+              :class="{ errorField: errors.lastName }"
+            />
+            <span v-if="errors.lastName" class="errorMessage">{{ errors.lastName }}</span>
           </div>
+
           <div class="form-item" id="email">
             <label for="emailAdress">Email Address *</label>
-            <input autocomplete="off" required type="email" name="email" id="emailAdress" />
+            <input
+              v-model="email"
+              autocomplete="off"
+              type="email"
+              name="email"
+              :class="{ errorField: errors.email }"
+            />
+            <span v-if="errors.email" class="errorMessage">{{ errors.email }}</span>
           </div>
         </div>
       </fieldset>
       <fieldset>
         <legend>Query Type *</legend>
         <div class="radio">
-          <div class="radio-group">
+          <div class="radio-group" :class="{ queryTypeError: errors.queryType }">
             <div class="radio-item">
-              <input
-                autocomplete="off"
-                required
-                type="radio"
-                id="general"
-                name="type"
-                value="General Enquiry"
-              />
+              <input v-model="queryType" type="radio" id="general" value="General Enquiry" />
               <label for="general">General Enquiry</label>
             </div>
             <div class="radio-item">
-              <input
-                autocomplete="off"
-                required
-                type="radio"
-                id="support"
-                name="type"
-                value="Support Request"
-              />
+              <input v-model="queryType" type="radio" id="support" value="Support Request" />
               <label for="support">Support Request</label>
             </div>
           </div>
+          <span v-if="errors.queryType" class="errorMessage">{{ errors.queryType }}</span>
         </div>
       </fieldset>
       <fieldset>
         <div class="form-item">
           <label for="message">Message *</label>
-          <textarea required name="message" id="message" rows="10" cols="30"></textarea>
+          <textarea
+            v-model="message"
+            name="message"
+            id="message"
+            rows="5"
+            :class="{ errorField: errors.message }"
+          ></textarea>
+          <span v-if="errors.message" class="errorMessage">{{ errors.message }}</span>
         </div>
       </fieldset>
       <fieldset>
         <div class="terms">
-          <input
-            autocomplete="off"
-            required
-            type="checkbox"
-            id="terms"
-            name="terms"
-            value="Consent"
-          />
-          <label for="terms">I consent to being contacted by the team *</label>
+          <div class="terms-wraps">
+            <input v-model="terms" type="checkbox" id="terms" name="terms" />
+            <label for="terms">I consent to being contacted by the team *</label>
+          </div>
+          <span v-if="errors.terms" class="errorMessage">{{ errors.terms }}</span>
         </div>
       </fieldset>
       <button type="submit">Submit</button>
@@ -83,7 +128,7 @@ textarea {
   border-radius: 8px;
   border: 1.3px solid var(--clr-green-500);
 }
-/* HOVER | ACTIVE | FOCUS */
+/* HOVER | ACTIVE | FOCUS | ERROR */
 input[type='text']:hover,
 input[type='email']:hover,
 .radio-item:hover,
@@ -191,6 +236,8 @@ textarea {
 }
 
 .terms {
+  display: flex;
+  flex-direction: column;
   input {
     margin-right: 0.5rem;
   }
@@ -203,6 +250,18 @@ button {
   font-weight: bold;
   color: var(--clr-white);
   background-color: var(--clr-green-600);
+}
+
+.errorMessage {
+  margin-top: 0.5rem;
+  color: var(--clr-red);
+}
+input.errorField,
+textarea.errorField {
+  border: 1.4px solid var(--clr-red);
+}
+.queryTypeError {
+  margin-bottom: 0.5rem;
 }
 
 @media (min-width: 37.5rem) {
